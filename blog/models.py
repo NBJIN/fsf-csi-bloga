7947008c33a9c5f78d4aa59bc1ef36d49c90afd6
noreen <106515976+NBJIN@users.noreply.github.com>
2023-01-14 21:22:17 +0000

    change to create model

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, timezone
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = (
+    (0, "Draft"), 
+    (1, "Published")
+    )
 
 # Create your models here.
 
@@ -12,11 +15,10 @@ class Create(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     contributor_create = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_create')
     date_of_post = models.DateTimeField(auto_created=True)
-    update_post = models.DateTimeField(auto_created=True)
+    update_post = models.DateTimeField(auto_now_add=True)
     image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     no_of_likes = models.ManyToManyField(User, related_name="blog_likes")
-    comment_create = models.TextField()
     excerpt = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
 
@@ -43,7 +45,7 @@ class Comment(models.Model):
         ordering = ['-date_of_comment']
 
     def __str__(self):
-        return self.title
+        return self.contributor_comment
 
     def no_of_comments(self):
         return self.comments.count()
